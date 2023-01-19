6f2e01a8b0c785aa9a52a5fe5df026713a077dc2
Matias Lenci <matiaslenci@gmail.com>
2023-01-19 12:22:19 -0300

    first commit add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    NotFoundPageComponent,
+
   ],
   imports: [
     BrowserModule,
