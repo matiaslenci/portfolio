0252cc6568ae36a3962fbd7022d4bbd380cc5a3a
Matias Lenci <matiaslenci@gmail.com>
2023-01-29 14:46:43 -0300

    fix link buttons header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  linkCv() {
+    window.open(
+      'https://drive.google.com/file/d/1oGvTmbScF3PGeH5aGZA5dXxwaM6fZl6X/view?usp=sharing',
+      '_blank'
+    );
+  }
+
+  linkIn() {
+    window.open(
+      'https://www.linkedin.com/in/matias-lenci-228828186/',
+      '_blank'
+    );
   }
 }
